290d5111fd45c2c61cc5d7f9f5f06458b979bdc2
Jared <jared.cooke@my.jcu.edu.au>
2017-03-13 11:05:23 +1000

    finished

---
+++ ascii.py
@@ -0,0 +1,17 @@
+def get_number(upper, lower):
+    finished = False
+    while not finished:
+        try:
+            number = int(input('Enter a number ({} - {}): '.format(lower, upper)))
+            if lower <= number <= upper:
+                return number
+            else:
+                print('Please enter a valid number')
+        except ValueError:
+            print('Please enter a number')
+
+UPPER_LIMIT = 127
+LOWER_LIMIT = 33
+number = get_number(UPPER_LIMIT, LOWER_LIMIT)
+print('The character for {} is {}'.format(number, chr(number)))
+
